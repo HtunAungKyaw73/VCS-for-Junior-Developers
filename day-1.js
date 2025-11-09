e226bdd74babfc6cbdf890e96245dfb74702104b
HtunAungKyaw73 <htunaungkyaw730@gmail.com>
2025-11-09 20:22:50 +0700

    delete: 2 line in day-1

--- day-1.js
+++ day-1.js
@@ -3,6 +3,3 @@ console.log("This is Day 1 of VCS Class!");
 
 // This is for branch testing
 console.log("Testing branches in version control systems.");
-
-// Another line to test merge conflicts
-console.log("Merge conflict test line.");
